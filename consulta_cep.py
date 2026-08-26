15c6af40bff0daa94b254de5f84937a04c74b212
RODRIGO MARCELINO DE OLIVEIRA <202601030728@alunos.ibmec.edu.br>
2026-08-26 17:31:02 -0300

    Exibe o endereço de forma legível

--- consulta_cep.py
+++ consulta_cep.py
@@ -5,4 +5,8 @@ cep = "01310930"
 url = f"https://viacep.com.br/ws/{cep}/json/" 
 resposta = requests.get(url) 
 dados = resposta.json() 
-print(dados)
+print("CEP:", dados["cep"]) 
+print("Rua:", dados["logradouro"]) 
+print("Bairro:", dados["bairro"]) 
+print("Cidade:", dados["localidade"]) 
+print("Estado:", dados["uf"]) 
